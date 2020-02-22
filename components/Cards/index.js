b0fcee675f8092a17e264f9108c8e52aa21a6411
Russell Smith <russellsmith@MacBook-Pro.local>
2020-02-21 20:46:05 -0500

    completed building the three components and answered questions

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,61 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function CardComponent(data) {
+  let card = document.createElement("div");
+  let cardHeadline = document.createElement("div");
+  let cardAuthor = document.createElement("div");
+  let cardImgContainer = document.createElement("div");
+  let cardImg = document.createElement("img");
+  let cardName = document.createElement("span");
+
+  // add main div card class
+  card.classList.add("card");
+
+  // add card headline class
+  cardHeadline.classList.add("headline");
+  cardHeadline.textContent = data.headline;
+
+  // add author class
+  cardAuthor.classList.add("author");
+
+  // add card image container class
+  cardImgContainer.classList.add("img-container");
+
+  // card image src
+  cardImg.src = data.authorPhoto;
+
+  // add auther name to span
+  cardName.textContent = `By ${data.authorName}`;
+
+  // add child img
+  cardImgContainer.appendChild(cardImg);
+
+  // add author child elements
+  cardAuthor.appendChild(cardImgContainer);
+  cardAuthor.appendChild(cardName);
+
+  // add child to main div
+  card.appendChild(cardHeadline);
+  card.appendChild(cardAuthor);
+
+  return card;
+}
+
+let cardsContainer = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(res => {
+    let articles = res.data.articles;
+    for (const prop in articles) {
+      console.log(articles[prop]);
+      articles[prop].forEach(article => {
+        cardsContainer.appendChild(CardComponent(article));
+      });
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  let header = document.createElement("div");
+  let headerDate = document.createElement("span");
+  let headerH1 = document.createElement("h1");
+  let headerTemp = document.createElement("span");
+
+  // add class to main div header
+  header.classList.add("header");
+
+  // add header date content
+  headerDate.classList.add("date");
+  headerDate.textContent = "SMARCH 28, 2019";
+
+  // add h1 content
+  headerH1.textContent = "Lambda Times";
+
+  // add header temp
+  headerTemp.classList.add("temp");
+  headerTemp.textContent = "98°";
+
+  header.appendChild(headerDate);
+  header.appendChild(headerH1);
+  header.appendChild(headerTemp);
+
+  return header;
+}
+
+let headerContainer = document.querySelector(".header-container");
+
+headerContainer.appendChild(Header());
